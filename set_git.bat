95a04a48c226b5742f14ff9af703ac4531d3424f
khalilmnsor <mnsorkhalel@gmail.com>
2024-08-02 02:31:38 +0300

    first commit

--- set_git.bat
+++ set_git.bat
@@ -8,5 +8,5 @@ git init
 git add .
 git commit -m "first commit"
 git branch -M main
-git remote add origin https://github.com/khalelmnsor/tergol-1.git
+git remote add origin https://github.com/[set your github repository path].git
 git push -u origin main
